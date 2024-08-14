588a0cea24cf33be59341f3357447bbbb26d6a24
Ahmed Ali <ahmedcali1984@gmail.com>
2024-08-14 06:31:27 +0300

    Add Tests

--- main.c
+++ main.c
@@ -2,41 +2,9 @@
 
 int main(void)
 {
-	init();
-
-	Person ahmed = {
-		.account_number = 2234567,
-		.age = 20,
-		.balance = 2346347,
-		.password = "ahmed123",
-		.name = "ahmed",
-		.gender = "M",
-	};
-
-	bool created = create_account(&ahmed);
-	if(!created)
-	{
-		return 1;
-	}
-
-	Person shay = {
-		.account_number = 234567,
-		.age = 22,
-		.balance = 2343747,
-		.password = "shay123",
-		.name = "shay",
-		.gender = "F",
-	};
-
-	bool created2 = create_account(&shay);
-	if(!created2)
-	{
-		return 1;
-	}	
+	Load_Accounts(STREAM);
 
 	choose_activity();
-
-	Print_accounts();
 
 	unload_Hash_table();
 
